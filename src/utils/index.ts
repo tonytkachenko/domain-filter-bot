acdf83f2f693035ad69395810beb5f61c89e687f
Tony Tkachenko <103267335+tonytkachenko@users.noreply.github.com>
2024-03-13 02:05:33 +0200

    fix: update utils and import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
-export { type Logger, logger } from "./logger.js";
+export * from "./check-html.js";
 export * from "./clean-domain.js";
+export { type Logger, logger } from "./logger.js";
 export * from "./read-files.js";
+export * from "./write-files.js";
--- src/utils/read-files.ts
+++ src/utils/read-files.ts
@@ -1,6 +1,6 @@
 import { parse } from "csv-parse";
-import * as Excel from "exceljs";
-import { createReadStream } from "node:fs";
+import ExcelJS from "exceljs";
+import fs from "node:fs";
 
 export async function readFirstColumnCsv(filePath: string): Promise<string[]> {
   return new Promise((resolve, reject) => {
@@ -10,10 +10,10 @@ export async function readFirstColumnCsv(filePath: string): Promise<string[]> {
       fromLine: 2,
     });
 
-    createReadStream(filePath)
+    fs.createReadStream(filePath)
       .pipe(parser)
       .on("data", (row) => {
-        results.push(row[0]); // Push the first column value
+        results.push(row[0]);
       })
       .on("end", () => resolve(results))
       .on("error", (error) => reject(error));
@@ -21,17 +21,15 @@ export async function readFirstColumnCsv(filePath: string): Promise<string[]> {
 }
 
 export async function readFirstColumnXlsx(filename: string): Promise<any[]> {
-  const workbookReader = new Excel.stream.xlsx.WorkbookReader(filename, {});
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const workbookReader = new ExcelJS.stream.xlsx.WorkbookReader(filename, {});
   const firstColumn: any[] = [];
 
   for await (const worksheetReader of workbookReader) {
     for await (const row of worksheetReader) {
-      // Get the value of the first cell in the row
       const firstCell = row.getCell(1);
       firstColumn.push(firstCell.value);
     }
-    break; // Only process the first worksheet
+    break;
   }
 
   return firstColumn;
